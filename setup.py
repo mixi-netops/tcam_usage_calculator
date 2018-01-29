6c20080cf3238bf1a0960067f9054dd45190a75b
hekki <hiroki.ito@mixi.co.jp>
2018-01-29 17:04:20 +0900

    tcam_usage_calculator init

---
+++ setup.py
@@ -0,0 +1,38 @@
+import io
+import os
+from setuptools import find_packages, setup
+
+# Package meta-data.
+NAME = 'tcam_usage_calculator'
+DESCRIPTION = 'Predict TCAM usage of router.'
+URL = 'https://github.com/xflagstudio/tcam_usage_calculator'
+EMAIL = 'hiroki.ito@mixi.co.jp'
+AUTHOR = 'hekki'
+REQUIRED = []
+
+here = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+about = {}
+with open(os.path.join(here, NAME, '__version__.py')) as f:
+    exec(f.read(), about)
+
+
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    entry_points={
+        'console_scripts': [
+            'tcam_usage_calculator=tcam_usage_calculator.main:main'
+            ],
+    },
+    install_requires=REQUIRED,
+    license='MIT'
+)
---
+++ tcam_usage_calculator/router/config.py
@@ -0,0 +1,66 @@
+import os
+import re
+
+
+class Config:
+    '''
+    path: String
+        router config filepath.
+    '''
+    def __init__(self, path):
+        self.path = path
+        self.read()
+
+    def read(self):
+        try:
+            f = open(self.path, 'r')
+        except FileNotFoundError as e:
+            print(e, 'error occurred')
+            quit(1)
+
+        config = f.read()
+        f.close()
+
+        self.raw = config
+
+    def getFirewallLineList(self):
+        firewallLineList = []
+
+        for line in self.raw.split('\n'):
+            if line.find('set firewall family inet') >= 0:
+                firewallLineList.append(line)
+
+        return firewallLineList
+
+    '''
+    Extract the term defined in a specific filter
+
+    filterName: String
+    termName: String
+    '''
+    def getTermLineList(self, filterName, termName):
+        termLineList = []
+
+        for line in self.getFirewallLineList():
+            pattern = f"{'^.*' + filterName + '.*' + termName + '.*$'}"
+            if not re.match(pattern, line):
+                continue
+            else:
+                termLineList.append(line)
+
+        return termLineList
+
+    '''
+    Extract the 'prefix-list' defined in a specific filter
+
+    prefixListName: String
+    '''
+    def getPrefixListLineList(self, prefixListName):
+        prefixListLineList = []
+        pattern = f"{'set policy-options prefix-list ' + prefixListName}"
+
+        for line in self.raw.split('\n'):
+            if line.find(pattern) >= 0:
+                prefixListLineList.append(line)
+
+        return prefixListLineList
