78eb869ea12640df882095fd770f98f5d621a9ea
hekki <hiroki.ito@mixi.co.jp>
2018-06-25 16:34:30 +0900

    change list name

--- tcam_usage_calculator/router/config.py
+++ tcam_usage_calculator/router/config.py
@@ -11,14 +11,14 @@ class Config:
         self.path = path
         self.raw = self.__read()
 
-        self.firewall_line_list = self.__init_firewall_line_list()
+        self.firewall_lines = self.__init_firewall_line_list()
 
         self.filter_name = None
         self.term_name = None
         self.prefix_list_name = None
 
-        self._term_line_list = []
-        self._prefix_list_line_list = []
+        self._term_lines = []
+        self._prefix_list_lines = []
 
     def __read(self):
         try:
@@ -39,28 +39,28 @@ class Config:
         return firewall_line_list
 
     @property
-    def term_line_list(self):
-        if self._term_line_list is not []:
-            self._term_line_list = []
+    def term_lines(self):
+        if self._term_lines is not []:
+            self._term_lines = []
 
         pattern = f"{'^.*' + self.filter_name + '.*' + self.term_name + '.*$'}"
 
-        for line in self.firewall_line_list:
+        for line in self.firewall_lines:
             if not re.match(pattern, line):
                 continue
             else:
-                self._term_line_list.append(line)
+                self._term_lines.append(line)
 
-        return self._term_line_list
+        return self._term_lines
 
     @property
     def prefix_list_line_list(self):
-        if self._prefix_list_line_list is not []:
-            self._prefix_list_line_list = []
+        if self._prefix_list_lines is not []:
+            self._prefix_list_lines = []
 
         pattern = f"{'set policy-options prefix-list ' + self.prefix_list_name}"
         for line in self.raw.split('\n'):
             if line.find(pattern) >= 0:
-                self._prefix_list_line_list.append(line)
+                self._prefix_list_lines.append(line)
 
-        return self._prefix_list_line_list
+        return self._prefix_list_lines
